46134931cd163d27164c3d826b3fc5ee6b3d1b54
ymchoi <ymchoi@olivendove.com>
2025-01-13 07:55:27 +0900

    Initial Commit

---
+++ Live555RtspPlayer2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Live555RtspPlayer2
+//
+//  Created by yumi on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
